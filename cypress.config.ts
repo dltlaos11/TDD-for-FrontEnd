4a24644de42624b6442bb415842b39b76b6b7b8f
dltlaos11 <dltlaos14@naver.com>
2024-03-23 23:37:23 +0900

    edit: Amplify 빌드 실패 수정

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "j75f1p",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "http://localhost:3000",
   },
 });
